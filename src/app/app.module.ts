0bbf9c6f68401d8c67936dbd477cf1242727edc7
Alvaro Gutierrez <algut0511@gmail.com>
2019-09-06 19:17:50 -0500

    primer cargue del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { SocialLoginModule, AuthServiceConfig } from "angularx-social-login";
+import {
+  GoogleLoginProvider,
+  FacebookLoginProvider
+} from "angularx-social-login";
+import { LoginComponent } from "./login/login.component";
+import { AppRoutes } from "./app.routes";
+import { HomeComponent } from './home/home.component';
+import { UserHomeComponent } from './user-home/user-home.component';
+
+let config = new AuthServiceConfig([
+  {
+    id: GoogleLoginProvider.PROVIDER_ID,
+    provider: new GoogleLoginProvider(
+      "69624866613-gm4m8k5ajael6vvnfvksp0o8hs0e8hc3.apps.googleusercontent.com"
+    )
+  },
+ 
+]);
+
+export function provideConfig() {
+  return config;
+}
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent, LoginComponent, HomeComponent, UserHomeComponent],
+  imports: [BrowserModule, AppRoutes, SocialLoginModule],
+  providers: [
+    {
+      provide: AuthServiceConfig,
+      useFactory: provideConfig
+    }
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/user-home/user-home.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'angularx-social-login';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-user-home',
+  templateUrl: './user-home.component.html',
+  styleUrls: ['./user-home.component.css']
+})
+export class UserHomeComponent implements OnInit {
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  ngOnInit() {
+  }
+  signOut(): void {
+    this.authService.signOut();
+    this.router.navigate(["/"]);
+  }
+
+}
